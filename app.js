4f0c5055abfaae1a50c9f6fdaaa6acf4f8c592ab
tumana777 <tumana991@gmail.com>
2022-10-15 14:52:22 +0400

    Begin javascript practise

---
+++ app.js
@@ -0,0 +1,8 @@
+const hour = document.getElementById('hour');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const amPm = document.getElementById('am-pm');
+
+function clock () {
+    let h = new Date().getHours()
+}
