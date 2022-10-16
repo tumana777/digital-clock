f18c7444d562b21e94f24693a56a786994c1444a
tumana <tumana991@gmail.com>
2022-10-16 23:54:39 +0400

    Finish digital clock project

--- app.js
+++ app.js
@@ -4,5 +4,28 @@ const seconds = document.getElementById('seconds');
 const amPm = document.getElementById('am-pm');
 
 function clock () {
-    let h = new Date().getHours()
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+
+    if(h > 12){
+        h = h - 12;
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hour.innerText = h;
+    minutes.innerText = m;
+    seconds.innerText = s;
+    amPm.innerText = ampm;
+
+    setTimeout(()=>{
+        clock()
+    },1000);
 }
+
+clock();
